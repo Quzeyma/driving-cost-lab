02bf4d577f853cde982b90d46b8799ac4f99fdea
Quzeyma <153734747+Quzeyma@users.noreply.github.com>
2023-12-28 14:23:18 -0500

    Add files via upload

---
+++ driving cost lab.py
@@ -0,0 +1,7 @@
+gas_efficiency = float(input())
+gas_cost = float(input())
+per_mile = gas_cost / gas_efficiency
+ten_mile = per_mile * 10
+fifty_mile = per_mile * 50
+fourh_mile = per_mile * 400
+print( ten_mile , fifty_mile , fourh_mile)
